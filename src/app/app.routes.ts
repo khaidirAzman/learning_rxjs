e71f316e5c1cc9470638808ee528661b8f9a37e5
khaidirAzman <khaidir.azman83@gmail.com>
2024-01-03 14:10:22 +0800

    add jquery, add more pages components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {HttpComponent} from "./http/http.component";
 import {NgModule} from "@angular/core";
+import {EventHandlerComponent} from "./event-handler/event-handler.component";
+import {StateManagementComponent} from "./state-management/state-management.component";
+import {WebsocketsComponent} from "./websockets/websockets.component";
 
 export const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'http', component:HttpComponent}
+  {path: 'http', component:HttpComponent},
+  {path: 'event', component:EventHandlerComponent},
+  {path: 'state', component:StateManagementComponent},
+  {path: 'websockets', component:WebsocketsComponent},
 ];
 
 @NgModule({
---
+++ src/app/event-handler/event-handler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-handler',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-handler.component.html',
+  styleUrl: './event-handler.component.css'
+})
+export class EventHandlerComponent {
+
+}
