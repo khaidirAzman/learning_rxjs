259a75a02e0356f3704b5212d9d34a43db7e8b65
khaidirAzman <khaidir.azman83@gmail.com>
2024-01-03 18:21:42 +0800

    use routerLink for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Router, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {CarouselComponent} from "./carousel/carousel.component";
 import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CarouselComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, CarouselComponent, HomeComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rxjsTutorial';
-  constructor(private router:Router) {
-  }
-  navigateToPage(path: string) {
-    this.router.navigate([path])
-      .then(function () {
-        console.log('success to Path: ', path);
-      }).catch(function (){
-      console.log('failure to Path: ', path);
-    });
+  constructor() {
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,8 +16,6 @@ export const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [],
-  bootstrap: []
+  exports: [RouterModule]
 })
 export class AppRoutes {}
